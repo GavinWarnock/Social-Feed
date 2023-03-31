5619070920720ef8fe4895344843703caa3081b9
Gavin Warnock <gbwarnock@gmail.com>
2023-03-31 14:01:16 -0600

    New: Established Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import NavBar from './Components/NavBar/NavBar';
+
+
 
 function App() {
+  
+  const [posts, setPosts] = useState([{name: "Test", thoughts: "Test text to show in box"}])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <NavBar />
       </header>
     </div>
   );
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import "./NavBar.css"
+
+const NavBar = (props) => {
+    return (
+        <nav>
+            <h2>InnerVoice</h2>
+        </nav>
+    );
+}
+
+export default NavBar
