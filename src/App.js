4644e5d83cbc862d6e72a7aa646d52d9543b4b62
Gavin Warnock <gbwarnock@gmail.com>
2023-04-03 13:06:57 -0600

    New: Added postList and imported it to App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { useState } from 'react';
 import NavBar from './Components/NavBar/NavBar';
+import PostList from './Components/PostList/PostList';
+
+
+function App(){
 
 
 
-function App() {
-  
-  const [posts, setPosts] = useState([{name: "Test", thoughts: "Test text to show in box"}])
+  const [posts, setPosts] = useState([{name: "Test", thoughts: "Test text to show in box"}]);
+
+function newPost(post){
+  let tempPost = [...posts, post];
+  setPosts(tempPost);
+
+
 
   return (
     <div>
       <header>
         <NavBar />
       </header>
+      <div className='container-fluid'>
+        <div className='edge'>
+          <PostList newPost={newPost}/>
+        </div>
+      </div>
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/Components/PostList/PostList.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const PostList = (props) => {
+
+    const [userName, setUserName] = useState('');
+    const [post, setPost] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newPost = {
+            userName: userName,
+            post: post
+        };
+        props.newPost(newPost)
+    }
+
+    return (
+        <form onSubmit={handleSubmit} classname='form grid'>
+            <div classname='form group' >
+                <label for='username'>User Name: </label>
+                <input type="text" class='form-control' id='username' placeholder='User Name' value={userName} onChange={(event) => setUserName(event.target.value)} />
+            </div>
+            <div className='form group'>
+                <label for='post'>Say what you want!</label>
+                <textarea class='form-control' id='post' rows='5' value={post} onChange={(event) => setPost(event.target.value)}/>
+            </div>
+            <button type='submit' className='btn btn-primary' style={{'margin-top': '1em', 'color': "orange"}}>Submit Post</button>
+        </form>
+    )
+}
+
+export default PostList
