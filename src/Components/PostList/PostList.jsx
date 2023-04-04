643b2fde96d77d3a0a31f6e7e79cc8d20a5ef9b4
Gavin Warnock <gbwarnock@gmail.com>
2023-04-04 15:25:56 -0600

    Improvement: Attempted to add like and dislike buttons

--- src/Components/PostList/PostList.jsx
+++ src/Components/PostList/PostList.jsx
@@ -2,7 +2,30 @@ import './PostList.css'
 import React, { useState } from 'react';
 
 
+
+
 const Posts = (props) => {
+    const [like, setLike] = useState('inactive')
+    const [dislike, setDislike] = useState('inactive')
+
+    function handleLike() {
+        if (like === 'inactive') {
+            setLike('active')
+        }
+        if (dislike === 'active') {
+            setDislike('inactive')
+        }
+    }
+
+    function handleDislike() {
+        if (dislike === 'inactive') {
+            setDislike('active')
+        }
+        if (like === 'active') {
+            setLike('inactive')
+        }
+    }
+    
     console.log(props)
     return (
         <div className="list-group">
@@ -16,6 +39,10 @@ const Posts = (props) => {
                         <div>
                             <p className="posts"><h3>{posts.post}</h3>
                             </p>
+                        </div>
+                        <div>
+                            <button className='like' onClick={handleLike}>Like</button>
+                            <button className='dislike' onClick={handleDislike}>Dislike</button>
                         </div>
                     </section>
                 )
